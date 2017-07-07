82e63696b7cba139ad5b93de4090a1dfde62b6d4
Jang Bahadur <jang@SIPL.WORK>
2017-07-07 14:25:59 +0530

    coursera project in progress

---
+++ app.js
@@ -0,0 +1,40 @@
+(function () {
+  'use strict';
+
+  angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+      //  $scope.lunchItems = "";
+
+
+        $scope.splitLunchItems = function () {
+
+        var separator = ",";
+        var messageHere = "";
+          var foodItems = $scope.lunchItems;
+          var arrayOfStrings = foodItems.split(separator);
+
+          if (foodItems == null || foodItems == "" ) {
+            messageHere ="Please enter data first";
+          }
+
+          if (foodItems == null || foodItems == "" ) {
+            messageHere ="Please enter data first";
+          }
+
+           if (arrayOfStrings.length <= 3 && arrayOfStrings.length > 0 && foodItems != "") {
+            messageHere ="Enjoy";
+          }
+
+          if (arrayOfStrings.length > 5) {
+            messageHere = "Too much!";
+          }
+
+                    $scope.message = messageHere;
+        }
+}
+
+})();
